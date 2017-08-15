e62f0c649a14ba11d8e116e7b09accc1cb8b4beb
Marcos Humberto Vieira (MARCOSHV) <marcoshv@tcu.gov.br>
2017-08-15 20:49:13 -0300

    aula 32, parcial

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,35 @@
   <div id="app">
     <img src="./assets/header_grass.png">
     <router-view></router-view>
+    <md-card>
+      <md-card-header>
+        <div class="md-title">
+          Material Design com Vue.js
+        </div>
+        <div class="md-subhead">
+          Com Marcos Vieira
+        </div>
+      </md-card-header>
+      <md-card-content>
+      </md-card-content>
+    </md-card>
     <img src="./assets/footer_grass.png">
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,6 +2,28 @@
   <div>
     <h1>Contato</h1>
     <h2>{{ msg }}</h2>
+
+    <md-card>
+      <md-card-media>
+        <img style="max-width: 400px; height: auto;" src="../assets/nos2.jpg" alt="People">
+      </md-card-media>
+
+      <md-card-header>
+        <div class="md-title">Família feliz</div>
+        <div class="md-subhead">Mexeu com um mexeu com todos !!!!</div>
+      </md-card-header>
+
+      <md-card-actions>
+        <md-button class="md-fab md-primary">
+          <md-icon>thumb_up</md-icon>
+        </md-button>
+      </md-card-actions>
+
+      <md-card-content>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+      </md-card-content>
+    </md-card>
+
   </div>
 </template>
 
